eb940c42abb96c139f32883bf18061005b0c9992
Tanmay <tanmayjajotar@gmail.com>
2022-07-21 17:43:50 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (41.10%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+   
+    public int climbStairs(int n) {
+        int[] arr = new int[n+1];
+        if(n<3) return n;
+        arr[1] = 1;
+        arr[2] = 2;
+        for(int i = 3; i <= n; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        return arr[n];
+    }
+    
+    
+}
