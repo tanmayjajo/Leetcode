e0ac25b9b0360a14002f11c0b0fa06a3cd795365
Tanmay <tanmayjajotar@gmail.com>
2022-08-05 18:57:30 +0530

    Time: 690 ms (51.92%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,4 +1,4 @@
 SELECT employee_id,
-IF (SUBSTRING(name, 1, 1) <> 'M' AND employee_id % 2 = 1, salary, 0) AS bonus
+IF (name NOT LIKE 'M%' AND employee_id % 2 = 1, salary, 0) AS bonus
 FROM Employees
 ORDER BY employee_id;
