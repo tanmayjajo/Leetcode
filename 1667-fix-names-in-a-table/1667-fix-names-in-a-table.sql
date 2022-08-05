6c4cd9f54b870f03fd3e3bd439fd1ae4a6a7ab57
Tanmay <tanmayjajotar@gmail.com>
2022-08-05 19:45:52 +0530

    Time: 650 ms (68.41%), Space: 0B (100.00%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,3 +1,3 @@
-SELECT user_id, CONCAT(UPPER(SUBSTR(name, 1,1)), LOWER(SUBSTR(name, 2))) AS name
+SELECT user_id, CONCAT(UPPER(SUBSTR(name, 1,1)), LOWER(SUBSTR(name, 2, LENGTH(name)))) AS name
 FROM Users
                                                         ORDER BY 1;
