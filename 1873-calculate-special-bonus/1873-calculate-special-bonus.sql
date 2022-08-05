4ef908700c9373eccd27d45dd38b505945912060
Tanmay <tanmayjajotar@gmail.com>
2022-08-05 19:18:37 +0530

    Time: 644 ms (60.25%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,4 +1,4 @@
-SELECT employee_id,
-IF (name NOT LIKE 'M%' AND employee_id % 2 = 1, salary, 0) AS bonus
+SELECT employee_id, 
+    salary*(employee_id % 2)*(name NOT LIKE 'M%') AS bonus
 FROM Employees
 ORDER BY employee_id;
