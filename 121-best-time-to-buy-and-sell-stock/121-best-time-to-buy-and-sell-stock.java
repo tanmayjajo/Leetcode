e4dcc4927afec3c9bddc66e5debcf4afe622ecfe
Tanmay <tanmayjajotar@gmail.com>
2022-08-13 15:32:31 +0530

    added comments

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,3 +1,12 @@
+// one traversal and maintaining the minimum number we get in the array, 
+// and also maintaining the maximum profit that we can get after selling at current index
+// when bought at the minimum index that we maintained.
+
+// maintain both and eventually we will return maximum profit that we maintained
+
+// initialising maxProfit with 0 because in the worst case of Descending Values, our profit will be 0 by default
+ 
+
 class Solution {
     public int maxProfit(int[] prices) {
         int mini = Integer.MAX_VALUE;
