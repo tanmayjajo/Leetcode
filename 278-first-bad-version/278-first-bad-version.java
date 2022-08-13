d5fb653872422c8652efe2b30b73cf70e9b73010
Tanmay <tanmayjajotar@gmail.com>
2022-08-13 16:02:38 +0530

    added comments

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -1,6 +1,10 @@
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
 
+
+// Use Binary Search
+// after checking the mid element also check the element just before it
+
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         if(n<2) return n;
