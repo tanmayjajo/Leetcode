777791c1f1979ac9c7a5aee7b6a6e4669a65db61
Tanmay <tanmayjajotar@gmail.com>
2022-09-04 17:25:08 +0530

    Time: 2 ms (36.19%), Space: 43.7 MB (47.45%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -15,28 +15,48 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
+        
+        // this is the final 2-D Matrix to be returned
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
         
+        
+        // Handling Edge Case
         if(root == null){
             return ans;
         }
         
+        // 
         Queue<TreeNode> queue = new LinkedList<>();
+        
+        
+            
         queue.add(root);
         
+        // traversing whole tree level by level
+        
         while(!queue.isEmpty()){
+            // list that contains each level
             List<Integer> list = new ArrayList<>();
             
             int size = queue.size();
+            
+            // adding all value of a particular level in the list
+            // adding all child nodes and preparing for next level
+            
             for(int i = 0; i < size; i++){
-                TreeNode temp = queue.peek();
+                
+                
+                // node that is to be added in list 
+                // before that we are storing its child
+                TreeNode temp = queue.poll();   
+                
                 if(temp.left != null){
                     queue.add(temp.left);
                 }
                 if(temp.right != null){
                     queue.add(temp.right);
                 }
-                queue.remove();
+                
                 list.add(temp.val);
             }
             
