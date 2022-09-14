572d271a4d7d54646bee32abe4f8c64cb95aeaf0
Tanmay <tanmayjajotar@gmail.com>
2022-09-14 18:30:55 +0530

    Time: 8 ms (29.63%), Space: 56.1 MB (6.89%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -11,43 +11,15 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        // find l1, l2
-        int L1 = len(headA); 
-        int L2 = len(headB);
-        
+        if(headA==null || headB==null) return null;
         ListNode temp1 = headA;
         ListNode temp2 = headB;
         
-        // acc of difference of l1 and l2, increase the pointer with greater L
-        if(L1>L2){
-            for(int i = 0; i < L1-L2; i++){
-                temp1=temp1.next;
-            }
-                
-        }else{
-            for(int i = 0; i < L2-L1; i++){
-                temp2=temp2.next;
-            }
+        while(temp1!=temp2){
+            temp1 = (temp1==null) ? headB : temp1.next;
+            temp2 = (temp2==null) ? headA : temp2.next;
         }
         
-        // linear traversal and booom
-        while(temp1!=null || temp2!=null){
-            if(temp1==temp2){
-                return temp1;
-            }
-            temp1 = temp1.next;
-            temp2 = temp2.next;
-        }
-        
-        return null;
-    }
-    public int len(ListNode head){
-        ListNode hehe = head;
-        int ans = 0;
-        while(hehe!=null){
-            hehe=hehe.next;
-            ans++;
-        }
-        return ans;
+        return temp1;
     }
 }
