1dfac8516cbb7f1fc4e7f2eb71de33dec09eafd6
Tanmay <tanmayjajotar@gmail.com>
2022-09-16 19:01:53 +0530

    Time: 1 ms (44.78%), Space: 42.7 MB (37.59%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,6 +1,29 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
         
+        while(mid  <= high){
+            if(nums[mid] == 0){
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            }else if(nums[mid] == 1){
+                mid++;
+            }else if(nums[mid] == 2){
+                swap(nums, high, mid);
+                high--;
+                
+            }
+        }
+    
+        
+    }
+    
+    public void swap(int[] nums, int a , int b){
+        int c = nums[a];
+        nums[a] = nums[b];
+        nums[b] = c;
     }
 }
