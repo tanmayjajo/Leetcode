567d88d97b25ba52ce40e505f7503e52da57331d
Tanmay <tanmayjajotar@gmail.com>
2022-10-19 10:33:11 +0530

    Time: 1 ms (96.87%), Space: 42.8 MB (44.72%) - LeetHub

--- 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.java
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.java
@@ -1,29 +1,10 @@
 class Solution {
     public int[] numberOfPairs(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int[] ans = new int[2];
-        
-        for(int i = 0; i < nums.length; i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i], map.get(nums[i]) + 1);
-            }else{
-                map.put(nums[i], 1);
-            }
+        int[] arr = new int[101];
+        int pairs = 0;
+        for(int n : nums){
+            pairs += ++arr[n]%2 == 0 ? 1 : 0;
         }
-        // long a = map.size() - map.values().stream().filter(isEvenNumber).count(); // a is number of leftovers
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()){
-            if(entry.getValue()%2 == 0){
-                ans[0] = ans[0] + (entry.getValue())/2;
-            }else{
-                if(entry.getValue() != 1){
-                    ans[0] = ans[0] + (entry.getValue()-1)/2;
-                }
-                ans[1]++;
-                
-            }
-            // System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
-        }
-             
-        return ans;
+        return new int[] {pairs, nums.length - pairs*2};
     }
 }
