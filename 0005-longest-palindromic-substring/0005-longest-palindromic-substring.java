5a2edaa200a73e8404729da6fa81342cafcff405
Tanmay <tanmayjajotar@gmail.com>
2023-01-01 21:23:15 +0530

    Time: 22 ms (88.36%), Space: 43 MB (55.59%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -5,34 +5,32 @@ class Solution {
         }
 
         int n = s.length();
-        boolean[][] dp = new boolean[n][n];
         String result = "";
 
-        // All substrings of length 1 are palindromes
         for (int i = 0; i < n; i++) {
-            dp[i][i] = true;
-            result = s.substring(i, i + 1);
-        }
-
-        // Check substrings of length 2
-        for (int i = 0; i < n - 1; i++) {
-            if (s.charAt(i) == s.charAt(i + 1)) {
-                dp[i][i + 1] = true;
-                result = s.substring(i, i + 2);
+            // Odd length palindromes
+            String odd = expandAroundCenter(s, i, i);
+            if (odd.length() > result.length()) {
+                result = odd;
             }
-        }
 
-        // Check substrings of length 3 and up
-        for (int k = 3; k <= n; k++) {
-            for (int i = 0; i < n - k + 1; i++) {
-                int j = i + k - 1;
-                if (dp[i + 1][j - 1] && s.charAt(i) == s.charAt(j)) {
-                    dp[i][j] = true;
-                    result = s.substring(i, j + 1);
-                }
+            // Even length palindromes
+            String even = expandAroundCenter(s, i, i + 1);
+            if (even.length() > result.length()) {
+                result = even;
             }
         }
 
         return result;
+    }
+
+    private String expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        // Substring excludes the last invalid character
+        return s.substring(left + 1, right);
     }
 }
