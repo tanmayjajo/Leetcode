b839474573eb93c2c87610b9a4cc8323f0a29ee7
Tanmay <tanmayjajotar@gmail.com>
2023-01-04 15:16:46 +0530

    Time: 85 ms (41.91%), Space: 104.3 MB (39.65%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -3,12 +3,8 @@ class Solution {
         
         HashMap<Integer, Integer> map = new HashMap<>();
         int count = 0;
-        for(int i = 0; i < tasks.length; i++){
-            if(!map.containsKey(tasks[i])){
-                map.put(tasks[i], 1);
-            }else{
-                map.put(tasks[i], map.get(tasks[i]) + 1);
-            }
+        for(int t: tasks){
+            map.put(t, map.getOrDefault(t, 0) + 1);
         }
         for(int value: map.values()){
             if(value == 1) return -1;
