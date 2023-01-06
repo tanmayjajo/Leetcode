0fd0ee6b1ac1f14aa7fae5cf68cb89e24ceff20c
Tanmay <tanmayjajotar@gmail.com>
2023-01-06 23:57:51 +0530

    Time: 99 ms (34.90%), Space: 61.2 MB (29.72%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,33 +1,31 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int totalSum = 0;
-        for(int i: nums){
-            totalSum += i;
+        int sum = 0;
+        for(int ele: nums){
+            sum += ele;
         }
-        if((totalSum&1) == 1) return false;
-        
-        int Smax = totalSum/2;
-        
-        boolean[][] t = new boolean[nums.length][Smax+1];
-        for(int j = 0; j < Smax+1; j++){
-            t[0][j] = false;
-        }
-        for(int i =0; i < nums.length; i++){
-            t[i][0] = true;
-        }
-        
-        
-        for(int i = 1; i < nums.length; i++){
-            for(int j = 0; j < Smax+1; j++){
-                if(nums[i-1] <= j){
-                    t[i][j] = t[i-1][j-nums[i-1]] || t[i-1][j];
-                }else{
-                    t[i][j] = t[i-1][j];
-                }
-            }
-        }
-        return t[nums.length-1][Smax];
+        if(sum%2 == 1) return false;
+        sum = sum/2;
+    
+        int[][] dp = new int[nums.length+5][sum+5];
+        Arrays.stream(dp).forEach(a -> Arrays.fill(a, -1));
+    
+        if(helper(nums, nums.length - 1, sum, dp) == sum) return true;
+        return false;
     }
     
-    
+    public int helper(int[] nums, int i, int sum, int[][] dp){
+        if(sum == 0) return 0;
+        if(i==0) return 0;
+        
+        
+        if(dp[i][sum] != -1) return dp[i][sum];
+        
+        if(nums[i] > sum){
+            return dp[i][sum] = helper(nums, i-1, sum,dp);
+        }
+        
+        return dp[i][sum] = Math.max(helper(nums, i-1, sum, dp), 
+                                     helper(nums, i-1, sum-nums[i], dp) + nums[i]);
+    }
 }
