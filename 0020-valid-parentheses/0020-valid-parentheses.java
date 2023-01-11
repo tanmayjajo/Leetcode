1a920423b773ee4f3acc2137c5bff9c55e405096
Tanmay <tanmayjajotar@gmail.com>
2023-01-11 13:46:22 +0530

    Time: 1 ms (99.54%), Space: 40.5 MB (71.37%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,26 +1,17 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
         
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == '('){
-                stack.add('(');
-            }else if(s.charAt(i) == '{'){
-                stack.add('{');
-            }else if(s.charAt(i) == '['){
-                stack.add('[');
-            }else if(stack.size() == 0){
-                return false;
-            }else if(s.charAt(i) == ')' && stack.peek() == '('){
-                stack.pop();
-            } else if(s.charAt(i) == '}' && stack.peek() == '{'){
-                stack.pop();
-            }else if(s.charAt(i) == ']' && stack.peek() == '['){
-                stack.pop();
-            }else return false;
+        Stack<Character> st = new Stack<>();
+        
+        for(char c: s.toCharArray()){
+            if(c=='('){
+                st.push(')');
+            }else if(c=='['){
+                st.push(']');
+            }else if(c=='{'){
+                st.push('}');
+            }else if(st.isEmpty() || c != st.pop()) return false;
         }
-        
-        if(stack.size() != 0) return false;
-        return true;
+        return st.isEmpty();
     }
 }
