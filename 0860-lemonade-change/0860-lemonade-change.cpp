fa328579e161dadf2b951f855963b352eeb66a1d
Tanmay <tanmayjajotar@gmail.com>
2023-04-20 14:14:34 +0530

    Time: 103 ms (85.92%), Space: 83.3 MB (82.47%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -4,14 +4,25 @@ public:
         int tenC = 0;
         int fiveC = 0;
         
-        for(auto val : bills){
-            if(val==5) fiveC++;
-            else if(val==10) tenC++, fiveC--;
-            else if(tenC) tenC--, fiveC--;
-            else fiveC-=3;
-            
-            if(fiveC<0) return false;
+        for(int i = 0; i < bills.size(); i++){
+            if(bills[i] == 5){
+                fiveC++;
+            }else if (bills[i] == 10){
+                tenC++;
+                if(fiveC > 0) fiveC--;
+                else return false;
+            }else{
+                if(tenC > 0 && fiveC > 0){
+                    tenC--;
+                    fiveC--;
+                }else if(fiveC > 2){
+                    fiveC -= 3;
+                }else{
+                    return false;
+                }
+            }
         }
+        
         return true;
     }
 };
