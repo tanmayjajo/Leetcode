96ad802e303f52e70de45db1cbbf3ef3b0ab97a5
Tanmay <tanmayjajotar@gmail.com>
2023-04-22 12:12:55 +0530

    Time: 32 ms (92.70%), Space: 7.6 MB (78.35%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,18 +1,17 @@
 class Solution {
-int dp[505][505];
-    
 public:
+    int dp[505][505];
+    
     int solve(int l, int r, string &s){
-        if(l >= r) return 0;
-        
         if(dp[l][r] != -1) return dp[l][r];
         
+        if(l >= r) return 0;
         int ans = 1e8;
         if(s[l] == s[r]){
             ans = solve(l+1, r-1, s);
         }else{
-            ans = min(ans, 1+solve(l+1, r, s));
-            ans = min(ans, 1+solve(l, r-1, s));
+            ans = min(ans, 1 + solve(l+1, r, s));
+            ans = min(ans, 1 + solve(l, r-1, s));          
         }
         return dp[l][r] = ans;
     }
