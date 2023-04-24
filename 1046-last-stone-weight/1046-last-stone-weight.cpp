481c4cb5075ea48bca5f696b986cd3b93e9d63e9
Tanmay <tanmayjajotar@gmail.com>
2023-04-24 11:23:57 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (41.71%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for(int i = 0; i < stones.size(); i++){
+            pq.push(stones[i]);
+        }
+        
+        while(pq.size() > 1){
+            int a = pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+            
+            
+            if(a != b){
+                pq.push(max(a, b) - min(a, b));
+            }
+        }
+        if(pq.size() == 0) return 0;
+        
+        return pq.top();
+    }
+};
