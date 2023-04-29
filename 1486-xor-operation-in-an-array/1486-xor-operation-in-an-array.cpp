82f79f14a68abedcc0b04b553b3ea44d9d6409d0
Tanmay <tanmayjajotar@gmail.com>
2023-04-29 22:25:11 +0530

    Time: 0 ms (100.00%), Space: 6 MB (73.13%) - LeetHub

---
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int ans = 0;
+        while(n--){
+            ans = ans^ (start + n*2);
+        }
+        return ans;
+    }
+};
