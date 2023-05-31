22383635d11c66ceb024645cd211ae2bf074ce00
Tanmay <tanmayjajotar@gmail.com>
2023-05-31 12:46:36 +0530

    Time: 132 ms (34.46%), Space: 31.3 MB (54.87%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+    // we'll go greedy
+    // use two pointer technique
+    
+    // traverse the whole string
+    // if '[' is found then move ahead expecting a ']' in future (for sure)
+    // if ']' is found and no prev '[' for it to be coupled then there is need to swap
+    // swap it with '[' from the end
+    
     int minSwaps(string s) {
         int n = s.size();
         
